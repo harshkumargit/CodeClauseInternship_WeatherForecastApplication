aa8bbec6371d3c9220d918cf85ab62bdbd8a1e74
Harsh Kumar <135570824+harshkumargit@users.noreply.github.com>
2023-08-04 19:48:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+// Function to update the clock and date
+function updateClockAndDate() {
+  const clockAndDateElement = document.getElementById('clockAndDate');
+  const now = new Date();
+  const time = now.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+  const date = now.toLocaleDateString();
+  clockAndDateElement.textContent = `${time} ${date}`;
+}
+
+// Update the clock and date every second
+setInterval(updateClockAndDate, 1000);
+
+// Rest of the existing JavaScript code
+// ...
+
+const API_KEY = '071d5ec7cdc74387e8f87f20f9fb183c';
+const weatherForm = document.getElementById('weatherForm');
+const cityInput = document.getElementById('cityInput');
+const weatherContainer = document.querySelector('.weather-container');
+const locationElement = document.querySelector('.location');
+const temperatureElement = document.querySelector('.temperature');
+const descriptionElement = document.querySelector('.description');
+
+// ...
+weatherForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const city = cityInput.value.trim();
+  if (city !== '') {
+    getWeatherData(city);
+  }
+});
+
+async function getWeatherData(city) {
+  try {
+    const weatherEndpoint = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURIComponent(city)}&units=metric&appid=${API_KEY}`;
+    const response = await fetch(weatherEndpoint);
+    const data = await response.json();
+
+    locationElement.textContent = data.name;
+    temperatureElement.textContent = `${data.main.temp} °C`;
+    descriptionElement.textContent = data.weather[0].description;
+  } catch (error) {
+    console.error('Error fetching weather data:', error);
+    locationElement.textContent = 'City not found';
+    temperatureElement.textContent = '';
+    descriptionElement.textContent = '';
+  }
+}
+
